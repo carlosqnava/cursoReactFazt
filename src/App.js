48ce37bc6ca973b1d166b4f4e8f2d09d1dedb5ed
carlosqnava <60723044+carlosqnava@users.noreply.github.com>
2021-07-28 16:24:29 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// function Hellow(props){
+//   return(
+//     <div>
+//       <h3>{props.subtitle}</h3>
+//       {props.mytext}
+//     </div>
+//   )
+// }
+
+class Hellow extends React.Component{
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show:!this.state.show})
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div>
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          <h1>No hay elementos</h1>
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      )
+    }
+  }
+  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      This is my component: 
+      <Hellow mytext="Hellow Fast" subtitle="lineas"/> 
+      <Hellow mytext="aaaa" subtitle="tiempo"/>  
+      <Hellow mytext="demasiadas" subtitle="subtítulo final"/>
     </div>
   );
 }
